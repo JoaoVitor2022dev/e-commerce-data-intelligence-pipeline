8450e6c9e6ef1e98f381f064b43f0c4d8d521de6
JoaoVitor2022dev <githubconta2021@gmail.com>
2026-06-17 00:24:45 -0300

    Description: Subindo a segunda parte das base de dados para o banco [200~olist_customers_dataset olist_geolocation_dataset olist_geolocation_dataset

--- SQL/CreateDataBase.sql
+++ SQL/CreateDataBase.sql
@@ -30,6 +30,60 @@ WITH
   CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
 );  
 
--- SUBINDO OS DADOS DA TABELA 
+-- SUBINDO OS DADOS DA TABELA olist_geolocation_dataset
+
+CREATE TABLE olist_geolocation_dataset (
+    geolocation_id INT IDENTITY(1,1) PRIMARY KEY,
+    geolocation_zip_code_prefix NVARCHAR(20),
+    geolocation_lat NVARCHAR(100),
+    geolocation_lng NVARCHAR(100),
+    geolocation_city NVARCHAR(50),
+    geolocation_state NVARCHAR(2)
+);
+
+BULK INSERT olist_geolocation_dataset 
+FROM 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\Projeto\Base de Dados\olist_geolocation_dataset.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+-- Subindo os dados da tabela olist_products_dataset 
+
+"","","product_description_lenght","product_photos_qty","product_weight_g","product_length_cm","product_height_cm","product_width_cm"
+
+CREATE TABLE olist_products_dataset (
+     product_id  VARCHAR(255) PRIMARY KEY,  
+     product_category_name VARCHAR(255),
+     product_name_lenght VARCHAR(2),
+     product_description_lenght VARCHAR(10), 
+     product_photos_qty   VARCHAR(5)
+     product_weight_g VARCHAR(10),
+     product_length_cm VARCHAR(10),
+     product_height_cm  VARCHAR(10),
+     product_width_cm VARCHAR(5)
+)
+
+BULK INSERT olist_products_dataset
+FROM 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\Projeto\Base de Dados\olist_products_dataset.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+
+
+
+
 
 
