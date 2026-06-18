7dd56f4cbcad520568fe92a41f75902c4936c5bd
JoaoVitor2022dev <githubconta2021@gmail.com>
2026-06-18 00:11:21 -0300

    feat(database): carga completa dos datasets Olist no banco de dados
    - Importado olist_customers_dataset
    - Importado olist_geolocation_dataset
    - Importado olist_products_dataset
    - Importado olist_sellers_dataset
    - Importado olist_orders_dataset
    - Importado olist_order_items_dataset
    - Importado olist_order_payments_dataset
    - Importado olist_order_reviews_dataset
    
    Todos os datasets foram carregados com sucesso e estão disponíveis para consultas no banco de dados.

--- SQL/CreateDataBase.sql
+++ SQL/CreateDataBase.sql
@@ -79,6 +79,135 @@ WITH
 )
 
 
+-- olist_sellers_dataset 
+
+CREATE TABLE olist_sellers_dataset (
+        seller_id VARCHAR(100),
+        seller_zip_code_prefix  VARCHAR(20),
+        seller_city VARCHAR(100),
+        seller_state VARCHAR(5)
+)
+
+BULK INSERT olist_products_dataset
+FROM 'caminho\para\olist_products_dataset.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+CREATE TABLE olist_orders_dataset (
+   order_id VARCHAR(50) ,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(50),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME
+)
+
+BULK INSERT olist_products_dataset
+FROM 'caminho\para\olist_orders_dataset.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+CREATE TABLE olist_order_items_dataset (
+    order_id VARCHAR(50) PRIMARY KEY,
+    order_item_id INT,
+    product_id VARCHAR(50),
+    seller_id VARCHAR(50),
+    shipping_limit_date DATETIME,
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2)
+);
+
+BULK INSERT olist_order_items_dataset
+FROM 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\Projeto\Base de Dados\olist_orders_dataset.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+CREATE TABLE olist_order_payments_dataset (
+    order_id VARCHAR(50),
+    payment_sequential INT,
+    payment_type VARCHAR(50),
+    payment_installments INT,
+    payment_value DECIMAL(10,2)
+);
+
+BULK INSERT olist_order_payments_dataset
+FROM 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\Projeto\Base de Dados\olist_order_payments_dataset.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+-- DROP TABLE olist_order_reviews_dataset
+CREATE TABLE olist_order_reviews_dataset (
+    review_id VARCHAR(50),
+    order_id VARCHAR(50),
+    review_score INT,
+    review_comment_title VARCHAR(100),
+    review_comment_message VARCHAR(MAX),
+    review_creation_date DATETIME,
+    review_answer_timestamp DATETIME
+);
+
+BULK INSERT olist_order_reviews_dataset
+FROM 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\Projeto\Base de Dados\olist_order_reviews_dataset.csv'
+WITH (
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              
+  FIELDTERMINATOR = ',',
+  FIELDQUOTE = '"',            -- Especifica que as colunas de texto estão entre aspas
+  ROWTERMINATOR = '\n',        
+  CODEPAGE = '65001'           
+);
+
+
+CREATE TABLE product_category_name_translation (
+     product_category_name VARCHAR(100), 
+     product_category_name_english VARCHAR(100)
+)
+
+BULK INSERT product_category_name_translation 
+FROM 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\Projeto\Base de Dados\product_category_name_translation.csv'
+WITH 
+(
+  FIRSTROW = 2,
+  FORMAT = 'CSV',              -- Esse comando mágico resolve o problema das aspas
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '0x0a',        -- Se der erro de linha, tente mudar para '0x0a'
+  CODEPAGE = '65001'           -- UTF-8 para não quebrar a acentuação
+)
+
+
+
+
+
 
 
 
